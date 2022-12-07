f727b4ddbb372f93fa24944cdda224c5f02edb73
kermo-git <saarse27@gmail.com>
2022-12-07 17:31:54 +0200

    Testing container deploy script

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp"
 export default () => {
 	return (
 		<div>
-			<h1>Hello World! This is the Container!</h1>
+			<h1>Hello World from Container!</h1>
 			<hr/>
 			<MarketingApp/>
 		</div>
